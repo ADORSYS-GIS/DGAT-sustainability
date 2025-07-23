07996826e9eff6870e2d1630eaa449fc3a258f04
christian_yemele <yemelechristian2@gmail.com>
2025-07-23 22:30:31 +0100

    modified oidc.ts

--- frontend/src/services/shared/oidc.ts
+++ frontend/src/services/shared/oidc.ts
@@ -4,8 +4,10 @@ export const oidcPromise = createOidc({
   issuerUri: import.meta.env.VITE_KEYCLOAK_ISSUER_URI,
   clientId: import.meta.env.VITE_KEYCLOAK_CLIENT_ID,
   homeUrl: import.meta.env.VITE_KEYCLOAK_HOME_URL,
-  silent_redirect_uri: "http://13.49.74.167/silent-renew.html",
-  post_logout_redirect_uri: "http://13.49.74.167/",
-  redirect_uri: "http://13.49.74.167/",
-
+  redirect_uri: import.meta.env.VITE_KEYCLOAK_REDIRECT_URI || import.meta.env.VITE_KEYCLOAK_HOME_URL,
+  silent_redirect_uri: import.meta.env.VITE_KEYCLOAK_HOME_URL + "/silent-renew.html",
+  post_logout_redirect_uri: import.meta.env.VITE_KEYCLOAK_HOME_URL,
+  silent_renew: true,
+  loadUserInfo: true,
+  automaticSilentRenew: true,
 });
