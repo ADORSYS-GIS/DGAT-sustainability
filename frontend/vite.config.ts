61a291ec367ec368bcf8f105211cb7d37f7cf4ea
Christian_Yemele <yemelechristian2@gmail.com>
2025-07-23 10:57:25 +0100

    chore: optimize Dockerfile, nginx config, and update dependencies
    - Simplified `Dockerfile.frontend` to reduce copy operations.
    - Enhanced `nginx.conf`:
      - Explicitly set MIME types for `.js` and `.css`.
      - Adjusted paths for consistent serving.
    - Updated `vite.config.ts` and `index.html` for relative paths.
    - Upgraded Postgres to version `16-alpine` in `docker-compose.yml`.

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,10 +5,12 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [
     react(),
     tsconfigPaths(),
     VitePWA({
+      base: './',
       srcDir: 'src',
       filename: 'custom-sw.js',
       strategies: 'injectManifest',
