148b59909391e9e9fef4ce93021361db0d773ba0
Christian_Yemele <yemelechristian2@gmail.com>
2025-07-23 20:28:36 +0100

    chore(auth): update OIDC config with static URIs for silent renew, redirect, and logout

--- frontend/src/services/shared/oidc.ts
+++ frontend/src/services/shared/oidc.ts
@@ -4,4 +4,8 @@ export const oidcPromise = createOidc({
   issuerUri: import.meta.env.VITE_KEYCLOAK_ISSUER_URI,
   clientId: import.meta.env.VITE_KEYCLOAK_CLIENT_ID,
   homeUrl: import.meta.env.VITE_KEYCLOAK_HOME_URL,
+  silent_redirect_uri: "http://13.49.74.167/silent-renew.html",
+  post_logout_redirect_uri: "http://13.49.74.167/",
+  redirect_uri: "http://13.49.74.167/",
+
 });
