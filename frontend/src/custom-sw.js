abf5f9a1ce44bef328abea11c907250ec10bb9a4
christian_yemele <yemelechristian2@gmail.com>
2025-07-23 07:34:33 +0100

    chore: comment out fetch handler in service worker and update configurations
    - Disabled `fetch` event listener in `custom-sw.js` for refinement.
    - Added `manifest.json` to support PWA setup.
    - Updated `docker-compose.yml`:
      - Added `CORS_ORIGIN` environment variable.
      - Adjusted Keycloak and API configuration.

--- frontend/src/custom-sw.js
+++ frontend/src/custom-sw.js
@@ -7,9 +7,9 @@ self.addEventListener('activate', (event) => {
   console.log('Service Worker activating.');
 });
 
-self.addEventListener('fetch', (event) => {
-  event.respondWith(fetch(event.request));
-});
+// self.addEventListener('fetch', (event) => {
+//   event.respondWith(fetch(event.request));
+// });
 
 // Import Workbox precaching and use the manifest
 import { precacheAndRoute } from 'workbox-precaching';
