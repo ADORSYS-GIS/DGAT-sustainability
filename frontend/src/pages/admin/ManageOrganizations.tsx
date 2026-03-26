1ceb2c17b55b9595aa432aee1fd00f32d25a2414
arielpetit <Ariel.Tchikaya@adorsys.com>
2026-03-26 11:47:17 +0100

    feat(): fixed the keycloak redirect url

--- frontend/src/pages/admin/ManageOrganizations.tsx
+++ frontend/src/pages/admin/ManageOrganizations.tsx
@@ -37,7 +37,7 @@ export const ManageOrganizations: React.FC = () => {
   const [formData, setFormData] = useState<OrganizationCreateRequest>({
     name: "",
     domains: [{ name: "" }],
-    redirectUrl: import.meta.env.VITE_ORGANIZATION_REDIRECT_URL || "https://ec2-63-177-64-129.eu-central-1.compute.amazonaws.com",
+    redirectUrl: import.meta.env.VITE_ORGANIZATION_REDIRECT_URL || "https://158.220.84.249:8443",
     enabled: "true",
     attributes: { categories: [] },
   });
@@ -141,7 +141,7 @@ export const ManageOrganizations: React.FC = () => {
     setFormData({ // Reset form data
       name: "",
       domains: [{ name: "" }],
-      redirectUrl: import.meta.env.VITE_ORGANIZATION_REDIRECT_URL || "https://ec2-63-177-64-129.eu-central-1.compute.amazonaws.com",
+      redirectUrl: import.meta.env.VITE_ORGANIZATION_REDIRECT_URL || "https://158.220.84.249:8443",
       enabled: "true",
       attributes: { categories: [] },
     });
@@ -162,7 +162,7 @@ export const ManageOrganizations: React.FC = () => {
           orgAttributes.redirect_url[0] &&
           !orgAttributes.redirect_url[0].includes("localhost"))
           ? orgAttributes.redirect_url[0]
-          : (import.meta.env.VITE_ORGANIZATION_REDIRECT_URL || "https://ec2-63-177-64-129.eu-central-1.compute.amazonaws.com"),
+          : (import.meta.env.VITE_ORGANIZATION_REDIRECT_URL || "https://158.220.84.249:8443"),
       enabled: ((orgAttributes.enabled && orgAttributes.enabled[0]) || "true") as "true" | "false",
       attributes: {
         categories: orgAttributes.categories || [],
